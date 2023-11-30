b0d8b9780e1c388341c07eb265f2e3b5025660be
Jonathon Ringeisen <ringeisen85@gmail.com>
2023-11-30 14:01:44 -0500

    Removed the image icon and replaced with first letter of users name.

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -133,11 +133,9 @@
                                 href="#"
                                 class="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-50"
                             >
-                                <img
-                                    class="h-8 w-8 rounded-full bg-gray-50"
-                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                    alt=""
-                                />
+                                <div class="flex justify-center items-center rounded-full bg-gray-200 border border-gray-400 w-7 h-7">
+                                    <p class="text-xl text-gray-600">{{ $page.props.auth.user.name[0] }}</p>
+                                </div>
                                 <span class="sr-only">Your profile</span>
                                 <span aria-hidden="true">{{ $page.props.auth.user.name }}</span>
                             </a>
