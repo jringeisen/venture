b33732463288c909f5ffe10dd12cce9c67845384
Jonathon Ringeisen <ringeisen85@gmail.com>
2023-12-01 08:53:30 -0500

    Updated the prompt seeders.

--- database/seeders/PromptSeeder.php
+++ database/seeders/PromptSeeder.php
@@ -14,12 +14,10 @@ class PromptSeeder extends Seeder
                 'category' => 'categorize',
                 'prompt' => <<<'EOT'
                 Analyze the question and categorize it using the provided Subject Category List. Respond in JSON format with 'subject' and 'sub_category' fields. Choose only from the listed categories:
-
                 - English: [Grammar and Syntax, Vocabulary, Reading Comprehension, Writing, Literature, Communication, Research Skills, Language and Culture]
                 - Mathematics: [Arithmetic, Algebra, Geometry, Trigonometry, Calculus, Statistics, Probability, Discrete Mathematics, Applied Mathematics]
                 - Science: [Physics, Chemistry, Biology, Earth Science, Environmental Science, Anatomy and Physiology, Geographical Sciences, Paleontology, Scientific Inquiry and Methodology]
                 - [Social Studies, Physical Education, Arts, Technology, Health Education, Career and Technical Education, Life Skills]
-
                 Do not create new categories.
                 EOT
             ],
@@ -45,16 +43,15 @@ class PromptSeeder extends Seeder
             [
                 'category' => 'moderation',
                 'prompt' => <<<'EOT'
-                Analyze the question and decide if it is an appropriate question for a 8-13 year old. If it's not, reply with the correct response below.
-                Return json with a key of flagged = True and a key of Message = "I understand you have a question, but the topic you're asking about is complex and requires guidance from a responsible adult. It's important to have these discussions in a safe and appropriate environment. I encourage you to talk to a trusted adult, such as a parent or guardian, who can guide you through a thoughtful and meaningful conversation. They can provide you with the guidance to address this question appropriately."
-                Flag the following content as inappropriate:
-                1. Hate: Content that expresses, incites, or promotes hate based on race, gender, ethnicity, religion, nationality, sexual orientation, disability status, or caste.
-                2. Hate/threatening: Hateful content that also includes violence or serious harm towards the targeted group.
-                3. Self-harm: Content that promotes, encourages, or depicts acts of self-harm, such as suicide, cutting, and eating disorders.
-                4. Sexual: Content meant to arouse sexual excitement, such as the description of sexual activity, or that promotes sexual services (excluding sex education and wellness).
-                5. Sexual/minors: Sexual content that includes an individual who is under 18 years old.
-                6. Violence: Content that promotes or glorifies violence or celebrates the suffering or humiliation of others.
-                7. Violence/graphic: Violent content that depicts death, violence, or serious physical injury in extreme graphic detail.
+                Review the question for suitability for ages 8-13. If inappropriate, flag it. Return JSON with 'flagged' = True and a 'Message' explaining why it's not suitable, advising to discuss with a trusted adult. Flag content involving:
+                1. Hate: Prejudice based on race, gender, ethnicity, religion, etc.
+                2. Hate/threatening: Hateful content with violence or harm.
+                3. Self-harm: Promotion or depiction of self-harm (e.g., suicide, eating disorders).
+                4. Sexual: Arousing or explicit sexual content, excluding educational material.
+                5. Sexual/minors: Sexual content involving under-18 individuals.
+                6. Violence: Promotion or glorification of violence.
+                7. Violence/graphic: Extremely graphic violent content.
+                Use this guidance strictly.
                 EOT
             ],
         ])->each(function ($prompt) {
