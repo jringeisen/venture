55da38402b4514d9e03df22992aad8fde366eb60
Jonathon Ringeisen <ringeisen85@gmail.com>
2023-12-04 10:25:30 -0500

    Fixed the layout of the database to be mobile friendly.

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,22 +4,20 @@
     <AuthenticatedLayout>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="flex space-x-3 w-full">
-                    <div class="space-y-3 w-1/3">
-                        <div class="bg-white h-32 dark:bg-gray-800 p-6 space-y-2 overflow-hidden shadow-sm sm:rounded-lg">
-                            <div class="text-gray-500 dark:text-gray-100">Total Questions</div>
-                            <p class="text-4xl font-bold">{{ totalQuestions }}</p>
-                        </div>
-
-                        <div class="bg-white h-32 p-6 dark:bg-gray-800 space-y-2 overflow-hidden shadow-sm sm:rounded-lg">
-                            <div class="text-gray-500 dark:text-gray-100">Daily Questions</div>
-                            <p class="text-4xl font-bold">{{ dailyQuestions }}</p>
-                        </div>
+                <div class="grid grid-cols-1 space-y-4 md:grid-cols-12 md:grid-rows-2 md:grid-flow-col md:space-y-0 md:gap-4">
+                    <div class="bg-white border dark:bg-gray-800 p-6 space-y-2 overflow-hidden shadow-sm sm:rounded-lg md:col-span-3">
+                        <div class="text-gray-500 dark:text-gray-100">Total Questions</div>
+                        <p class="text-4xl font-bold">{{ totalQuestions }}</p>
                     </div>
 
-                    <div class="w-2/3 bg-white p-6 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div class="text-gray-500 dark:text-gray-100">Subjects</div>
-                        <apexchart width="100%" type="pie" :options="options" :series="series"></apexchart>
+                    <div class="bg-white border p-6 dark:bg-gray-800 space-y-2 overflow-hidden shadow-sm sm:rounded-lg md:col-span-3">
+                        <div class="text-gray-500 dark:text-gray-100">Daily Questions</div>
+                        <p class="text-4xl font-bold">{{ dailyQuestions }}</p>
+                    </div>
+
+                    <div class="relative bg-white p-6 border dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg md:row-span-2 md:col-span-9">
+                        <div class="absolute text-gray-500 dark:text-gray-100">Subjects</div>
+                        <apexchart width="100%" height="100%" type="pie" :options="options" :series="series"></apexchart>
                     </div>
                 </div>
             </div>
