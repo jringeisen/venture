99cf2fea1894d5a0f228d09279f6e252178946a9
Jonathon Ringeisen <ringeisen85@gmail.com>
2023-12-06 08:55:43 -0500

    Updated the students dashboard.

--- app/Http/Controllers/Student/DashboardController.php
+++ app/Http/Controllers/Student/DashboardController.php
@@ -3,15 +3,22 @@
 namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
+use App\Models\PromptAnswer;
+use App\Services\PieChartService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request, PieChartService $pieChartService)
     {
         return Inertia::render('Student/Dashboard', [
+            'totalQuestions' => $request->user()->promptQuestions()->count(),
+            'dailyQuestions' => $request->user()
+                ->promptQuestions()
+                ->filterByDate(today()->toDateString())
+                ->count(),
             'promptsWithCounts' => $request->user()->promptAnswers()
                 ->whereNotNull('subject_category')
                 ->select('student_id', 'subject_category', DB::raw('count(*) as total'))
@@ -19,6 +26,11 @@ class DashboardController extends Controller
                 ->get()
                 ->pluck('total', 'subject_category')
                 ->toArray(),
+            'pieChartData' => $pieChartService
+                ->data(PromptAnswer::class, 'subject_category', $request->user()->id)
+                ->labels('subject_category')
+                ->series('total')
+                ->get(),
         ]);
     }
 }
