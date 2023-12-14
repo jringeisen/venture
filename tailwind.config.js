581196b115babc1febc4b74c439b4ae0942a6442
Jonathon Ringeisen <ringeisen85@gmail.com>
2023-12-14 09:56:17 -0500

    Updated the colors on mindful sidebar.

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,11 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                'primary-yellow': '#FFCC00',
+                'primary-gray': '#2B2B2B',
+                'primary-dark-gray': '#1E1E21',
+            },
         },
     },
 
