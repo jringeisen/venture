b3e27da2d5f3f39f44b1ac419d4e71da454ef0ee
Jonathon Ringeisen <ringeisen85@gmail.com>
2023-12-15 21:46:13 -0500

    Made some tweaks to fix issues with timezones.

--- app/Http/Resources/StudentResource.php
+++ app/Http/Resources/StudentResource.php
@@ -3,6 +3,8 @@
 namespace App\Http\Resources;
 
 use App\Models\PromptQuestion;
+use Carbon\Carbon;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -10,22 +12,25 @@ class StudentResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
-        $date = $request->date ?? now()->toDateString();
-
         return [
             'id' => $this->id,
             'name' => $this->name,
             'current_streak' => $this->current_streak,
             'prompt_questions' => $this->promptQuestions()
                 ->whereHas('promptAnswer')
-                ->filterByDate($date)
+                ->when($request->input('date', now($request->user()->timezone)->toDateString()), function (Builder $query, string $date) {
+                    $startOfDayUtc = Carbon::parse($date, auth()->user()->timezone)->startOfDay()->utc();
+                    $endOfDayUtc = Carbon::parse($date, auth()->user()->timezone)->endOfDay()->utc();
+
+                    $query->whereBetween('created_at', [$startOfDayUtc, $endOfDayUtc]);
+                })
                 ->get()
                 ->map(function (PromptQuestion $promptQuestion) use ($request) {
                     return [
                         'id' => $promptQuestion->id,
                         'question' => $promptQuestion->question,
                         'prompt_answer' => $promptQuestion->promptAnswer,
-                        'created_at' => $promptQuestion->created_at->setTimezone($request->user()->timezone)->toFormattedDateString(),
+                        'created_at' => $promptQuestion->created_at->timezone($request->user()->timezone)->toFormattedDateString(),
                     ];
                 }),
         ];
--- app/Services/MotivationalMessageService.php
+++ app/Services/MotivationalMessageService.php
@@ -3,7 +3,6 @@
 namespace App\Services;
 
 use App\Models\Student;
-use Illuminate\Support\Carbon;
 use OpenAI\Laravel\Facades\OpenAI;
 
 class MotivationalMessageService
