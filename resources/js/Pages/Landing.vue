52fe1b708d3a6cecb81468b383e617388b83509a
Jonathon Ringeisen <ringeisen85@gmail.com>
2023-12-21 20:55:11 -0500

    Fixed the styling of some dark mode styles.

--- resources/js/Pages/Landing.vue
+++ resources/js/Pages/Landing.vue
@@ -6,20 +6,20 @@
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
                     <a
                         href="/login"
-                        class="scale-100 p-6 bg-white dark:bg-primary-gray dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-primary-yellow"
+                        class="scale-100 p-6 bg-primary-gray ring-1 ring-inset ring-white/5 rounded-lg shadow-gray-500/20 shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-primary-yellow"
                     >
                         <div>
-                            <div class="h-16 w-16 bg-yellow-50 dark:bg-neutral-700 flex items-center justify-center rounded-full">
+                            <div class="h-16 w-16 bg-neutral-700 flex items-center justify-center rounded-full">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 stroke-primary-yellow">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
                                 </svg>
                             </div>
 
-                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">
+                            <h2 class="mt-6 text-xl font-semibold text-white">
                                 Are you a teacher?
                             </h2>
 
-                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                            <p class="mt-4 text-gray-400 text-sm leading-relaxed">
                                 Click here to login as a teacher.
                             </p>
                         </div>
@@ -29,7 +29,7 @@
                             fill="none"
                             viewBox="0 0 24 24"
                             strokeWidth="1.5"
-                            class="self-center shrink-0 stroke-[#FFCC00] w-8 h-8 mx-6"
+                            class="self-center shrink-0 stroke-primary-yellow w-8 h-8 mx-6"
                         >
                             <path
                                 strokeLinecap="round"
@@ -41,20 +41,20 @@
 
                     <a
                         href="/student/login"
-                        class="scale-100 p-6 bg-white dark:bg-primary-gray dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-primary-yellow"
+                        class="scale-100 p-6 bg-primary-gray ring-1 ring-inset ring-white/5 rounded-lg shadow-gray-500/20 shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-primary-yellow"
                     >
                         <div>
-                            <div class="h-16 w-16 bg-yellow-50 dark:bg-neutral-700 flex items-center justify-center rounded-full">
+                            <div class="h-16 w-16 bg-neutral-700 flex items-center justify-center rounded-full">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 stroke-[#FFCC00]">
                                     <path stroke-linecap="round" stroke-linejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
                                 </svg>
                             </div>
 
-                            <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">
+                            <h2 class="mt-6 text-xl font-semibold text-white">
                                 Are you a student?
                             </h2>
 
-                            <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                            <p class="mt-4 text-gray-400 text-sm leading-relaxed">
                                 Click here to login as a student.
                             </p>
                         </div>
