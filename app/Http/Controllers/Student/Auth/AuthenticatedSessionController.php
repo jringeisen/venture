ac37802d4906868bcdcbf6544822f2c471923b5b
Jonathon Ringeisen <ringeisen85@gmail.com>
2024-01-02 20:53:50 -0500

    wip

--- app/Http/Controllers/Student/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Student/Auth/AuthenticatedSessionController.php
@@ -19,7 +19,7 @@ class AuthenticatedSessionController extends Controller
         return Inertia::render('Auth/Student/Login', [
             'canResetPassword' => Route::has('password.request'),
             'status' => session('status'),
-            'username' => $request->username,
+            'username' => $request->input('username') ?? null
         ]);
     }
 
