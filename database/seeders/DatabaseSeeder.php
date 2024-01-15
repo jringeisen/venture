10b849ee3934af2d3649a32696c3526f0b20467d
Jonathon Ringeisen <ringeisen85@gmail.com>
2024-01-14 21:25:19 -0500

    Updated the readme with instructions and configuration. Updated the DatabaseSeeder and the StudentSeeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,6 +19,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             StudentSeeder::class,
             PromptSeeder::class,
+            TimezoneSeeder::class,
+            QuestionSeeder::class,
         ]);
     }
 }
