2cf63922a5c7fd041784be5c7d91986798c26a62
Jonathon Ringeisen <ringeisen85@gmail.com>
2024-01-17 21:23:11 -0500

    Removed cacheFor methods that aren't being used.

--- app/Nova/Metrics/Dashboards/Trends/PromptQuestionsCreated.php
+++ app/Nova/Metrics/Dashboards/Trends/PromptQuestionsCreated.php
@@ -34,16 +34,6 @@ class PromptQuestionsCreated extends Trend
     }
 
     /**
-     * Determine for how many minutes the metric should be cached.
-     *
-     * @return \DateTimeInterface|\DateInterval|float|int
-     */
-    public function cacheFor()
-    {
-        // return now()->addMinutes(5);
-    }
-
-    /**
      * Get the URI key for the metric.
      *
      * @return string
