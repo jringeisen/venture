36f87d34bee3ec8f389ca1c1502a534283b652dc
Justin Werner <justin.t.werner@gmail.com>
2024-01-19 19:20:16 -0600

    Attendance requests

--- app/Http/Controllers/Student/StudentController.php
+++ app/Http/Controllers/Student/StudentController.php
@@ -3,16 +3,39 @@
 namespace App\Http\Controllers\Student;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StudentAttendanceRequest;
 use App\Models\Student;
+use App\Services\StudentAttendanceService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
+    public function __construct(private readonly StudentAttendanceService $attendanceService)
+    {}
+
     public function update(Request $request, Student $student): RedirectResponse
     {
         $student->update($request->only('motivational_message'));
 
         return to_route($request->redirect_route);
+    }
+
+    public function persistActivity(StudentAttendanceRequest $request): array
+    {
+        $data = $request->validated();
+
+        $this->attendanceService->persist($data['totalSeconds']);
+
+        return ['message' => 'Okay'];
+    }
+
+    public function updateActivity(StudentAttendanceRequest $request): array
+    {
+        $data = $request->validated();
+
+        $this->attendanceService->update($data['totalSeconds']);
+
+        return ['message' => 'Okay'];
     }
 }
