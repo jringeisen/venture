3c67b81bf484ad1f51a0bfb239c1a9ba8a4646dc
Justin Werner <justin.t.werner@gmail.com>
2024-01-19 19:18:43 -0600

    Javascript watchers for student activity.

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -288,4 +288,48 @@ const showBetaPricing = () => {
         && !page.props.auth.isSubscribed
         && page.url !== '/subscription-checkout-options';
 }
+
+
+let startDate = new Date();
+let elapsedTime = 0;
+
+const focus = function() {
+    startDate = new Date();
+};
+
+const blur = function() {
+    const endDate = new Date();
+    const spentTime = endDate.getTime() - startDate.getTime();
+
+    elapsedTime += spentTime;
+
+    axios.post(
+        '/student/activity/update',
+        { totalSeconds: elapsedTime / 1000 }
+    );
+};
+
+const beforeunload = function() {
+    const endDate = new Date();
+    const spentTime = endDate.getTime() - startDate.getTime();
+
+    elapsedTime += spentTime;
+
+    if (elapsedTime && elapsedTime > 0) {
+        axios.post(
+            '/student/activity/persist',
+            { totalSeconds: elapsedTime / 1000 }
+        );
+    }
+};
+
+const createStudentActiveListeners = () => {
+    window.addEventListener('focus', focus);
+    window.addEventListener('blur', blur);
+    window.addEventListener('beforeunload', beforeunload);
+}
+
+if (page.props.auth.type === 'student') {
+    createStudentActiveListeners();
+}
 </script>
