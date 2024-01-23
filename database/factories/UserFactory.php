3975c9103c7227952006a4758b25f3d7d8a75b92
Jonathon Ringeisen <ringeisen85@gmail.com>
2024-01-22 21:08:59 -0500

    - Refactored StudentSeeder. (#42)
    - Removed PortStudentsToUsersSeeder.
    - Fixed failing test.
    - Created a parent and student state in the UserFactory.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,37 +2,45 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'timezone' => 'America/New_York',
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    public function parent(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'parent_id' => null,
+            'email' => fake()->unique()->safeEmail(),
+        ]);
+    }
+
+    public function student(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'parent_id' => User::factory()->parent()->create()->id,
+            'username' => fake()->userName(),
+            'grade' => fake()->numberBetween(1, 12),
+            'age' => fake()->numberBetween(6, 18),
         ]);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,16 +4,14 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        User::factory()->create([
+        User::factory()->parent()->create([
             'name' => 'Admin',
             'email' => 'admin@test.com',
-            'password' => Hash::make('password'),
         ]);
 
         $this->call([
