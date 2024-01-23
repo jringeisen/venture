f0d6bd0b6ad118664a3500e66074daff0c2fd9a6
Jonathon Ringeisen <ringeisen85@gmail.com>
2024-01-23 17:12:36 -0500

    Implement Sentry For Error Reporting (#43)
    * - Removed flare
    
    * - Implemented Sentry via the documentation.

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,13 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import * as Sentry from '@sentry/vue';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN_PUBLIC,
+});
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
