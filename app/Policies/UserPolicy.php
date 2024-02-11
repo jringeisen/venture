76112b9eb315afb3153eb861edcc1a71f8f444d8
Jonathon Ringeisen <ringeisen85@gmail.com>
2024-02-10 20:22:46 -0500

    Fixed some issues in the admin panel.

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -9,18 +9,21 @@ class UserPolicy
     public function view(User $authUser, User $user): bool
     {
         return $authUser->id === $user->parent_id
-            || $authUser->id === $user->id;
+            || $authUser->id === $user->id
+            || in_array($authUser->email, config('app.nova_admin_emails'));
     }
 
     public function update(User $authUser, User $user): bool
     {
         return $authUser->id === $user->parent_id
-            || $authUser->id === $user->id;
+            || $authUser->id === $user->id
+            || in_array($authUser->email, config('app.nova_admin_emails'));
     }
 
     public function delete(User $authUser, User $user): bool
     {
         return $authUser->id === $user->parent_id
-            || $authUser->id === $user->id;
+            || $authUser->id === $user->id
+            || in_array($authUser->email, config('app.nova_admin_emails'));
     }
 }
