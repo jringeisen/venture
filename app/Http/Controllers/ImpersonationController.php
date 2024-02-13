d56b7c9fcf1f2c1d7d45838de7f1cabc5c6b2dc9
Jonathon Ringeisen <ringeisen85@gmail.com>
2024-02-13 08:53:54 -0500

    wip

--- app/Http/Controllers/ImpersonationController.php
+++ app/Http/Controllers/ImpersonationController.php
@@ -7,14 +7,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Nova\Contracts\ImpersonatesUsers;
 
-class StartImpersonationController extends Controller
+class ImpersonationController extends Controller
 {
-    public function __invoke(Request $request, User $user, ImpersonatesUsers $impersonator)
+    public function start(Request $request, User $user, ImpersonatesUsers $impersonator)
     {
         $this->authorize('view', $user);
 
         $impersonator->impersonate($request, Auth::guard(), $user);
 
         return redirect(route('student.prompts.index'));
+    }
+
+    public function stop(Request $request, ImpersonatesUsers $impersonator)
+    {
+        $impersonator->stopImpersonating($request, Auth::guard(), User::class);
+
+        return redirect('/');
     }
 }
